48ce3569043349ab4bf5ba5fb24c591580420570
Rusic <rusic@fastmail.com>
2026-05-24 13:42:46 +0200

    init

---
+++ .prettierrc.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    semi: true,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+    plugins: [],
+};
+
+export default config;
