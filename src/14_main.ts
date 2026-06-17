a81538b33687ef257a11b77d3eb49e9410b46501
Rusic <rusic@fastmail.com>
2026-06-17 16:30:37 +0200

    add fragment shader for advanced noise effects and update material settings in 14_main.ts

--- src/14_main.ts
+++ src/14_main.ts
@@ -15,8 +15,102 @@ renderer.setPixelRatio(window.devicePixelRatio);
 document.querySelector('#app')?.appendChild(renderer.domElement);
 
 const bgGeometry = new THREE.PlaneGeometry(200, 200);
-const bgMaterial2 = new THREE.MeshLambertMaterial({ color: 0x020e3d });
-const bgMesh = new THREE.Mesh(bgGeometry, bgMaterial2);
+
+const fragmentShader = `
+    #ifdef GL_ES
+    precision mediump float;
+    #endif
+    
+    uniform vec3 iResolution;
+    uniform float iTime;
+    
+    float random (in vec2 _st) {
+        return fract(sin(dot(_st.xy,
+                             vec2(12.9898,78.233)))*
+                             43758.5453123);
+    }
+    
+    // Based on Morgan McGuire @morgan3d
+    // https://www.shadertoy.com/view/4dS3Wd
+    float noise (in vec2 _st) {
+        vec2 i = floor(_st);
+        vec2 f = fract(_st);
+    
+        // Four corners in 2D of a tile
+        float a = random(i);
+        float b = random(i + vec2(1.0, 0.0));
+        float c = random(i + vec2(0.0, 1.0));
+        float d = random(i + vec2(1.0, 1.0));
+    
+        vec2 u = f * f * f * (3.0 - 2.0 * f);
+    
+        return mix(a, b, u.x) +
+                (c - a)* u.y * (1.0 - u.x) +
+                (d - b) * u.x * u.y;
+    }
+    
+    #define NUM_OCTAVES 5
+    
+    float fbm ( in vec2 _st) {
+        float v = 0.0;
+        float a = 0.5;
+        vec2 shift = vec2(100.0);
+        // Rotate to reduce axial bias
+        mat2 rot = mat2(cos(0.5), sin(0.5),
+                        -sin(0.5), cos(0.5));
+        for (int i = 0; i < NUM_OCTAVES; ++i) {
+            v += a * noise(_st);
+            _st = rot * _st * 2.0 + shift;
+            a *= 0.5;
+        }
+        return v;
+    }
+    
+    void main() {
+        vec2 st = gl_FragCoord.xy/iResolution.xy * 4.;
+        // st += st * abs(sin(iTime*0.1)*3.0);
+        vec3 color = vec3(0.0);
+    
+        vec2 q = vec2(0.);
+        q.x = fbm( st + 0.02*iTime);
+        q.y = fbm( st + vec2(1.0));
+    
+        vec2 r = vec2(0.);
+        r.x = fbm( st + 1.0*q + vec2(1.7,9.2)+ 0.15*iTime );
+        r.y = fbm( st + 1.0*q + vec2(8.3,2.8)+ 0.126*iTime);
+    
+        float f = fbm(st+r);
+        
+        color = mix(vec3(0.0745, 0.4863, 0.6471), 
+                    vec3(0.0745, 0.0983, 0.1171),
+                    clamp(f*f * 4.0, 0.0, 1.0));
+    
+        color = mix(color,
+                    vec3(0.0345, 0.0203, 0.0471),
+                    clamp(length(q),0.0,1.0));
+
+        color = mix(color,
+                    vec3(0.0745, 0.4863, 0.6471),
+                    clamp(length(r.x),0.0,1.0));
+    
+        gl_FragColor = vec4((f*f*f + .3*f*f + .7*f)*color,1.);
+    }
+`;
+
+const uniforms = {
+    iTime: { value: 0 },
+    iResolution: { value: new THREE.Vector3() },
+};
+
+uniforms.iResolution.value.set(window.innerWidth, window.innerHeight, 1);
+const material2 = new THREE.ShaderMaterial({
+    fragmentShader,
+    uniforms,
+    side: THREE.DoubleSide,
+});
+
+const bgMaterial2 = new THREE.MeshLambertMaterial({ color: 0x020e3d, side: THREE.DoubleSide });
+const bgMesh = new THREE.Mesh(bgGeometry, material2);
 bgMesh.position.z = -3;
 scene.add(bgMesh);
 
@@ -25,16 +119,16 @@ const loader = new GLTFLoader();
 let torus: THREE.Mesh | undefined;
 
 const settings = {
-    wireframe: true,
-    transmission: 0,
+    wireframe: false,
+    transmission: 1,
     roughness: 0,
-    thickness: 0.818,
-    ior: 1.0605,
-    reflectivity: 0,
-    emissiveIntensity: 0.352,
-    emissiveColor: '#172a78',
-    flatShading: true,
-    metalness: 0,
+    thickness: 1.04,
+    ior: 1.9095,
+    reflectivity: 0.938,
+    emissiveIntensity: 0.548,
+    emissiveColor: '#11053b',
+    flatShading: false,
+    metalness: 0.323,
     directionalLight: true,
     pointLight1: true,
     pointLight2: true,
@@ -171,6 +265,8 @@ renderer.setAnimationLoop(() => {
     const now = performance.now();
     const phase = now % cycleDuration;
     const cycleIndex = Math.floor(now / cycleDuration);
+
+    uniforms.iTime.value = now / 1000;
 
     if (torus) {
         if (cycleIndex !== lastCycle) {
